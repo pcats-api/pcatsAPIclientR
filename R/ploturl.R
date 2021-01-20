c804036a4c65f73ec4b38ca4228ae9b6c911a3e3
Michal Kouril <xmkouril@gmail.com>
2021-01-19 23:58:33 -0500

    Added type parameter to ploturl

--- R/ploturl.R
+++ R/ploturl.R
@@ -2,12 +2,14 @@
 #' @title Return plot URL
 #' @description Return plot URL
 #' @param jobid Job ID of the previously submitted job
+#' @param type Plot type
 #' @return url
 #' @export
 #' @import httr
 #' @import utils
 #'
-ploturl <- function(jobid) {
-  output<-paste(readLines(paste0("https://pcats.research.cchmc.org/api/job/",jobid,"/ploturl")),"\n")
+ploturl <- function(jobid, type="") {
+  if (type!="") { type<-paste0("/",type) }
+  output<-paste(readLines(paste0("https://pcats.research.cchmc.org/api/job/",jobid,"/ploturl",type)),"\n")
   output
 }
