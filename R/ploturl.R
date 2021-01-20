27672466cbf04309063d6166d5d7ab17ca5af877
Michal Kouril <xmkouril@gmail.com>
2021-01-20 11:20:37 -0500

    Fixed ploturl function.

--- R/ploturl.R
+++ R/ploturl.R
@@ -2,14 +2,15 @@
 #' @title Return plot URL
 #' @description Return plot URL
 #' @param jobid Job ID of the previously submitted job
-#' @param type Plot type
+#' @param plottype Plot type
 #' @return url
 #' @export
 #' @import httr
 #' @import utils
 #'
-ploturl <- function(jobid, type="") {
-  if (type!="") { type<-paste0("/",type) }
-  output<-paste(readLines(paste0("https://pcats.research.cchmc.org/api/job/",jobid,"/ploturl",type)),"\n")
+ploturl <- function(jobid, plottype="") {
+  if (plottype!="") { plottype<-paste0("/",plottype) }
+  url<-jsonlite::fromJSON(paste0("https://pcats.research.cchmc.org/api/job/",jobid,"/ploturl"))
+  output<-paste0(url$url,plottype)
   output
 }
