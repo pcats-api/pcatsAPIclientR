5741c9d3bb2488eda8b2bad8e0ea51bd19e3976e
chen-chen130 <chen_chen130@hotmail.com>
2021-03-31 11:30:58 -0400

    Update dynamicGP.cate.R

--- R/dynamicGP.cate.R
+++ R/dynamicGP.cate.R
@@ -1,4 +1,4 @@
-#' @title Get conditional average treatment effect for heterogeneous computation data with two time points.
+#' @title Get conditional average treatment effect for data with two time points.
 #' @description Estimate the conditional average treatment effect of user-specified treatment groups.
 #' @details The contrast of potential outcomes for the reference group and the treatment group is estimated at a list of x values if x is not a factor. If x is a factor, the conditional average treatment effect is estimated at each value of levels of x.
 #' @note The conditional average treatment effect is estimated based on the sample data. The observations with missing covariates in the model are excluded. For the unspecified variables in the model, the observed data is used to estimate the conditional average treatment effect.
@@ -6,10 +6,8 @@
 #' @param x The name of variable which may have the heterogeneous treatment effect. x should be a categorical variable.
 #' @param control.tr A vector of the values of the treatment variables at all stages as the reference group.
 #' @param treat.tr A vector of the values of the treatment variables at all stages compared to the reference group.
-#' @param pr.values pr.values
-#' @return Return an object of class "cate.effect" which contains a list of the following components:
-#'   \item{out}{A data frame containing the posterior samples of the estimated conditional average treatment effect.}
-#'   \item{hte.est}{The statistical summary results of the conditional average treatment effect}
+#' @param pr.values An optional vector of user-defined values of c for PrCTE.
+#' @return jobid
 #' @export
 #'
 dynamicGP.cate <- function(jobid,x,control.tr,treat.tr,pr.values=NULL) {
